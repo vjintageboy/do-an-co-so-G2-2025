af515a6790d90c6e6cadb91da2ac811c44118192
ducanhgit2004 <nducanh485@gmail.com>
2025-05-28 01:05:27 +0700

    homepage

---
+++ netflexx/src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from 'react'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+
+
+
+
+const TitleCards = ({title, category}) => {
+
+  const cardsRef = useRef();
+
+const handlewheel = (event)=>{
+  event.preventDefault();
+  cardsRef.current.scrollLeft += event.deltaY;
+}
+
+useEffect(()=>{
+  cardsRef.current.addEventListener('wheel', handlewheel)
+},[])
+
+  return (
+    <div className='title-cards'>
+      <h2>{title?title:"Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default TitleCards
