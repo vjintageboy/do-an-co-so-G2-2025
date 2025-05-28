ffc5f458f63a147544b95f8e223fce1aa17d02d9
ducanhgit2004 <nducanh485@gmail.com>
2025-05-28 23:23:01 +0700

    login+responsive

---
+++ netflexx/src/firebase.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRHO2nmWLv6EzsUtgcLyur9WxvE7WYcoU",
+  authDomain: "netflix-4a36d.firebaseapp.com",
+  projectId: "netflix-4a36d",
+  storageBucket: "netflix-4a36d.firebasestorage.app",
+  messagingSenderId: "423154110093",
+  appId: "1:423154110093:web:1ae5315bb65f9a50b69cc4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password)=>{
+    try {
+        const res =  await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "users"),{
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const login = async (email, password)=>{
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const logout = ()=>{
+    signOut(auth);
+}
+
+export {auth, db, signup, login, logout};
