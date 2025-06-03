fe1ebc4b11e9b1dc0a4180c29033971d4cd07c5f
Hoai Nam <22010017@st.phenikaa-uni.edu.vn>
2025-06-03 07:21:51 +0700

    fix date

--- netflexx/src/components/Footer/Footer.jsx
+++ netflexx/src/components/Footer/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
         <li>Corporate Information</li>
         <li>Contact Us</li>
       </ul>
-      <p className='copyright-text'>© 1997-2013 Netflix, Inc.</p>
+      <p className='copyright-text'>© 1997-2025 Netflix, Inc.</p>
     </div>
   )
 }
